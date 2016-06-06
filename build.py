f5ef5333795d42612bed2f92f07622a56503caf2
Ian <ianfan0@gmail.com>
2016-06-06 08:25:16 +0100

    Minor refactoring

--- build.py
+++ build.py
@@ -111,18 +111,18 @@ for subject in subjects:
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <title>{}</title>
     <link href="/alevel/style.css" rel="stylesheet">
     <script src="/alevel/script.js" async></script>
     <script type="text/x-mathjax-config">
         MathJax.Ajax.config.path["Contrib"] = "//cdn.mathjax.org/mathjax/contrib";
         MathJax.Hub.Config({{
             jax: ["input/TeX", "output/NativeMML"],
-            TeX: {{ extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"] }},
-            extensions: ["tex2jax.js","[Contrib]/siunitx/siunitx.js"]
+            TeX: {{ extensions: ["AMSmath.js", "AMSsymbols.js", "noErrors.js", "noUndefined.js"] }},
+            extensions: ["tex2jax.js", "[Contrib]/siunitx/siunitx.js"]
         }});
     </script>
     <script src="//cdn.mathjax.org/mathjax/latest/MathJax.js" async></script>
-    <title>{}</title>
   </head>
   <body>
     <nav class="{}">
